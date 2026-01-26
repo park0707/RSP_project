de8e9921b01c9e8e2d9a805b690493d2e1072f32
박현준 <phj20030707@gmail.com>
2026-01-26 19:28:50 +0900

    Add vercel Analytics component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router.tsx'
 import './index.css'
-
+import { Analytics } from '@vercel/analytics/react'; 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router}/>
+    <Analytics />
   </StrictMode>,
 )
